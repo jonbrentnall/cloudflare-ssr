eca04a0869de0fbf8e5f2302b9847d8e7e9e9990
Jon Brentnall <19314205+jonbrentnall@users.noreply.github.com>
2023-10-16 15:24:40 +0100

    log status

--- app/page.js
+++ app/page.js
@@ -22,22 +22,22 @@ async function getData() {
 export default async function Home() {
   const { isEnabled } = draftMode();
   const pageRequest = getPageRequest({ includeDrafts: isEnabled });
-  const { data } = await getData(pageRequest);
+  const data = await getData(pageRequest);
 
   console.log(isEnabled);
 
   // if (isEnabled) {
-  //   return (
-  //     <RealtimeHomepageData
-  //       subscription={{
-  //         ...pageRequest,
-  //         initialData: data,
-  //         token: process.env.NEXT_DATOCMS_API_TOKEN,
-  //       }}
-  //     />
-  //   );
+    return (
+      <RealtimeHomepageData
+        subscription={{
+          ...pageRequest,
+          initialData: data,
+          token: process.env.NEXT_DATOCMS_API_TOKEN,
+        }}
+      />
+    );
   // }
 
-  return <p>{data.home.title}</p>
+  // return <p>{data.home.title}</p>
 
 }
--- components/Homepage/index.js
+++ components/Homepage/index.js
@@ -3,7 +3,31 @@
 import { useQuerySubscription } from 'react-datocms';
 
 export default function RealtimeHomepageData({ subscription }) {
-  const { data } = useQuerySubscription(subscription);
+    const { data, error, status } = useQuerySubscription(subscription);
 
-  return <p>{data.home.title}</p>;
+    const statusMessage = {
+      connecting: 'Connecting to DatoCMS...',
+      connected: 'Connected to DatoCMS, receiving live updates!',
+      closed: 'Connection closed',
+    };
+
+    return (
+        <div>
+      <p>Connection status: {statusMessage[status]}</p>
+      {error && (
+        <div>
+          <h1>Error: {error.code}</h1>
+          <div>{error.message}</div>
+          {error.response && (
+            <pre>{JSON.stringify(error.response, null, 2)}</pre>
+          )}
+        </div>
+      )}
+      {data && (
+        <div>{JSON.stringify(data, null, 2)}</div>
+      )}
+    </div>
+    )
+
+//   return <p>{data.home.title}</p>;
 }
