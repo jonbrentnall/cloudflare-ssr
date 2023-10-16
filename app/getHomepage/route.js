b6cce51e17d24a813215a3f080aa78164b5cda90
Jon Brentnall <19314205+jonbrentnall@users.noreply.github.com>
2023-10-16 11:14:57 +0100

    routing

---
+++ app/getHomepage/route.js
@@ -0,0 +1,18 @@
+import { draftMode } from 'next/headers';
+import { performRequest } from 'lib/datocms';
+
+export const runtime = 'edge';
+
+export async function getHomepageData() {
+    const { isEnabled } = draftMode();
+    const homePageQuery = `
+        query HomePage {
+            home {
+                title
+            }
+        }`;
+
+    const data = await performRequest({ query: homePageQuery, includeDrafts: isEnabled, });
+
+    return data;
+  }
--- app/page.js
+++ app/page.js
@@ -1,20 +1,7 @@
-import { draftMode } from 'next/headers';
-import { performRequest } from 'lib/datocms';
-
-async function getData() {
-  const { isEnabled } = draftMode();
-  const homePageQuery = `
-    query HomePage {
-      home {
-        title
-      }
-    }`;
-  const res = await performRequest({ query: homePageQuery, includeDrafts: isEnabled, });
-  return res;
-}
+import { getHomepageData } from './getHomepage/route';
 
 export default async function Home() {
-  const { home } = await getData();
+  const { home } = await getHomepageData();
   return (
     <p>{home.title}</p>
   )
