828858c1620536be2086063800051ede870938ce
Jon Brentnall <19314205+jonbrentnall@users.noreply.github.com>
2023-10-17 13:37:22 +0100

    lets look for a cookie instead

--- app/api/draft/route.js
+++ app/api/draft/route.js
@@ -3,6 +3,7 @@ import { draftMode } from 'next/headers'
 export const runtime = 'edge';
 
 export async function GET(request) {
-  draftMode().enable()
-  return new Response('Draft mode is enabled')
+  draftMode().enable();
+
+  return new Response('Draft mode is enabled');
 }
--- app/draftModeEnabled/route.js
+++ app/draftModeEnabled/route.js
@@ -1,11 +1,11 @@
-import { draftMode } from 'next/headers';
+import { draftMode, cookies } from 'next/headers';
 
 export const runtime = 'edge';
 
 export async function draftModeEnabled() {
-    const { isEnabled } = draftMode();
-
-    console.log("isEnabled", isEnabled);
+    // const { isEnabled } = draftMode();
+    const cookieStore = cookies()
+    const isEnabled = cookieStore.has('__prerender_bypass')
 
     return isEnabled;
   }
