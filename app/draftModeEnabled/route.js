0329f99a7fdc5396807c43080fb06560ad3a96a0
Jon Brentnall <19314205+jonbrentnall@users.noreply.github.com>
2023-10-17 14:19:24 +0100

    just checking

--- app/draftModeEnabled/route.js
+++ app/draftModeEnabled/route.js
@@ -3,9 +3,9 @@ import { draftMode, cookies } from 'next/headers';
 export const runtime = 'edge';
 
 export async function draftModeEnabled() {
-    // const { isEnabled } = draftMode();
-    const cookieStore = cookies()
-    const isEnabled = cookieStore.has('__prerender_bypass')
+    const { isEnabled } = draftMode();
+    // const cookieStore = cookies()
+    // const isEnabled = cookieStore.has('__prerender_bypass')
 
     return isEnabled;
   }
